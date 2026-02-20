76aae2178df8afc89d0489cf53c7608b65b907f1
fami0110 <masandofami@gmail.com>
2026-02-20 08:52:25 +0700

    Jobsheet 2: Percobaan 1

---
+++ Jobsheet_2/Mahasiswa14.java
@@ -0,0 +1,34 @@
+public class Mahasiswa14 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+    
+    void tampilkanInformasi() {
+        System.out.println("Nama: "+ nama);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        if (0.0 <= ipk && ipk <= 4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0!");
+        }
+    }
+
+    String nilaiKinerja(double ipk) {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else  {
+            return "Kinerja kurang";
+        }
+    }
+}
