a16db0ccce61e747c66936231135d39bb7314d20
fami0110 <masandofami@gmail.com>
2026-04-05 17:17:31 +0700

    obsheet 6: Percobaan 4.2

--- Jobsheet_6/MahasiswaDemo14.java
+++ Jobsheet_6/MahasiswaDemo14.java
@@ -41,7 +41,7 @@ public class MahasiswaDemo14 {
 		list.selectionSort();
 		list.tampil();
 
-		System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC): ");
+		System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (DESC): ");
 		list.insertionSort();
 		list.tampil();
 
