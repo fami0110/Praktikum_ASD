67858838f5506c137bd8281f7d44f10da47453cb
fami0110 <masandofami@gmail.com>
2026-05-08 09:11:36 +0700

    Jobsheet 11: Percobaan 1.2

--- Jobsheet_11/SLLMain.java
+++ Jobsheet_11/SLLMain.java
@@ -1,19 +1,39 @@
+import java.util.Scanner;
+
 public class SLLMain {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
 		SingleLinkedList14 sll = new SingleLinkedList14();
+		
+		Mahasiswa14 mhs[] = new Mahasiswa14[4];
+		String nim, nama, kelas;
+		double ipk;
 
-		Mahasiswa14 mhs1 = new Mahasiswa14("24212200", "Alvaro", "1A", 4.0);
-		Mahasiswa14 mhs2 = new Mahasiswa14("23212201", "Bimon", "2B", 3.8);
-		Mahasiswa14 mhs3 = new Mahasiswa14("22212202", "Cintia", "3C", 3.5);
-		Mahasiswa14 mhs4 = new Mahasiswa14("21212203", "Dirga", "4D", 3.6);
+		for (int i = 0; i < 4; i++) {
+			System.out.println("==============================");
+			System.out.println("Masukkan data mahasiswa ke-" + (i+1));
+			System.out.print("Nama	: ");
+			nama = sc.nextLine();
+			System.out.print("NIM	: ");
+			nim = sc.nextLine();
+			System.out.print("Kelas	: ");
+			kelas = sc.nextLine();
+			System.out.print("IPK	: ");
+			ipk = sc.nextDouble();
+			sc.nextLine();
+			mhs[i] = new Mahasiswa14(nim, nama, kelas, ipk);
+		}
+
+		System.out.println("");
 
 		sll.print();
-		sll.addFirst(mhs4);
+		sll.addFirst(mhs[3]);
 		sll.print();
-		sll.addLast(mhs1);
+		sll.addLast(mhs[0]);
 		sll.print();
-		sll.insertAfter("Dirga", mhs3);
-		sll.insertAt(2, mhs2);
+		sll.insertAfter("Dirga", mhs[2]);
+		sll.insertAt(2, mhs[1]);
 		sll.print();
 	}
 }
